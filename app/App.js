fddcc65486cf92985cfe9a112f4f07a8ae6e541c
Thihan Winhtet <thihanwinhtet1999@gmail.com>
2025-09-09 14:27:05 +0630

    Initial Setup

---
+++ app/App.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
