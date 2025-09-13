273bb100273158d503fe0aed85c233d9ba71bc81
Thihan Winhtet <thihanwinhtet1999@gmail.com>
2025-09-14 03:47:17 +0630

    Add icons to each screens

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import HomeScreen from "../screens/HomeScreen";
 import BrowseScreen from "../screens/BrowseScreen";
@@ -13,11 +14,71 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Browse" component={BrowseScreen} />
-        <Tab.Screen name="Community" component={CommunityScreen} />
-        <Tab.Screen name="Bookmark" component={BookmarkScreen} />
-        <Tab.Screen name="Account" component={AccountScreen} />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused, color, size }) => (
+              <MaterialCommunityIcons
+                name={`home${!focused ? "-outline" : ""}`}
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+          name="Home"
+          component={HomeScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused, color, size }) => (
+              <MaterialCommunityIcons
+                name={`compass${!focused ? "-outline" : ""}`}
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+          name="Browse"
+          component={BrowseScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused, color, size }) => (
+              <MaterialCommunityIcons
+                name={`crown${!focused ? "-outline" : ""}`}
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+          name="Engine"
+          component={CommunityScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused, color, size }) => (
+              <MaterialCommunityIcons
+                name={`bookmark${!focused ? "-outline" : ""}`}
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+          name="Bookmark"
+          component={BookmarkScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused, color, size }) => (
+              <MaterialCommunityIcons
+                name={`account-circle${!focused ? "-outline" : ""}`}
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+          name="Account"
+          component={AccountScreen}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
