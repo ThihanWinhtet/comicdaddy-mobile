55055335c3c724bc3f8a73c94e569435a1ecf8e7
Thihan Winhtet <thihanwinhtet1999@gmail.com>
2025-09-14 17:03:58 +0630

    Setup PopularComic-Hook

--- app/features/comics/hooks.js
+++ app/features/comics/hooks.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getComics } from "./api";
+import { getComics, getPopularComics } from "./api";
 
 export const useComics = (limit = 12) => {
   const [comics, setComics] = useState([]);
@@ -13,6 +13,24 @@ export const useComics = (limit = 12) => {
     setLoading(true);
     const response = await getComics(limit);
     if (response.ok) setComics(response.data.data.comics);
+    setLoading(false);
+  };
+
+  return { comics, loading, reload: loadComics };
+};
+
+export const usePopularComics = () => {
+  const [comics, setComics] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loadComics();
+  }, []);
+
+  const loadComics = async () => {
+    setLoading(true);
+    const response = await getPopularComics();
+    if (response.ok) setComics(response.data.data);
     setLoading(false);
   };
 
