cb61180c9531765653f116247815abc1672db5e0
Thihan Winhtet <thihanwinhtet1999@gmail.com>
2025-09-17 05:09:49 +0630

    Create BrowseScreen

--- app/screens/BrowseScreen.js
+++ app/screens/BrowseScreen.js
@@ -1,12 +1,120 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import {
+  MaterialCommunityIcons,
+  AntDesign,
+  FontAwesome6,
+} from "@expo/vector-icons";
 
-const BrowseScreen = () => {
+import Search from "../components/Search";
+import Screen from "../components/screen";
+
+const Browse = () => {
   return (
-    <View>
-      <Text>BrowseScreen</Text>
-    </View>
-  )
-}
+    <Screen>
+      {/* Search Component */}
+      <Search />
 
-export default BrowseScreen
+      {/* Search by item Component */}
+      <ScrollView>
+        <View style={styles.mainContainer}>
+          <Text style={styles.title}>History</Text>
+          <View style={styles.container}>
+            <TouchableOpacity style={styles.item}>
+              <Text>hentai</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+              <Text>big boobs</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+              <Text>overwatch</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+              <Text>overwatch</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+              <Text>overwatch</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+              <Text>overwatch</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+              <Text>overwatch</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+              <Text>overwatch</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={styles.mainContainer}>
+          <Text style={styles.title}>Status</Text>
+          <View style={styles.container}>
+            <TouchableOpacity style={styles.item}>
+              <MaterialCommunityIcons name="flag-outline" size={15} />
+              <Text>hentai</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+              <MaterialCommunityIcons name="flag-checkered" size={15} />
+              <Text>big boobs</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={styles.mainContainer}>
+          <Text style={styles.title}>Status</Text>
+          <View style={styles.container}>
+            <TouchableOpacity style={styles.item}>
+              <AntDesign name="dingding" size={15} />
+              <Text>hentai</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+              <MaterialCommunityIcons name="flower-outline" size={15} />
+              <Text>big boobs</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+              <FontAwesome6 name="gripfire" size={15} />
+              <Text>overwatch</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {/* Comics Grid */}
+      </ScrollView>
+    </Screen>
+  );
+};
+
+export default Browse;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    marginHorizontal: 15,
+    marginTop: 10,
+    marginBottom: 15,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  container: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    gap: 10,
+    marginTop: 10,
+  },
+  item: {
+    backgroundColor: "lightgray",
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+});
